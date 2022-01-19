526470f9e0cb563efb8f4fc561649ada76d81d4b
depotte6 <depotte6@gmail.com>
2022-01-19 06:11:08 -0500

    cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,69 @@
-const FILES_TO_CACHE = [
+var CACHE_NAME = "my-site-cache-v1";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+var urlsToCache = [
+  "/",
+  "/db.js",
+  "/index.js",
+  "/manifest.json",
+  "/styles.css",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
+  "https://cdn.jsdelivr.net/npm/chart.js@2.8.0"
+];
+
+self.addEventListener("install", function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      console.log("Opened cache");
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", function(event) {
+  // cache all get requests to /api routes
+  if (event.request.url.includes("/api/")) {
+    event.respondWith(
+      caches.open(DATA_CACHE_NAME).then(cache => {
+        return fetch(event.request)
+          .then(response => {
+            // If the response was good, clone it and store it in the cache.
+            if (response.status === 200) {
+              cache.put(event.request.url, response.clone());
+            }
+
+            return response;
+          })
+          .catch(err => {
+            // Network request failed, try to get it from the cache.
+            return cache.match(event.request);
+          });
+      }).catch(err => console.log(err))
+    );
+
+    return;
+  }
+
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return caches.match(event.request).then(function(response) {
+        if (response) {
+          return response;
+        } else if (event.request.headers.get("accept").includes("text/html")) {
+          // return the cached home page for all requests for html pages
+          return caches.match("/");
+        }
+      });
+    })
+  );
+});
+
+
+
+
+/*const FILES_TO_CACHE = [
     '/',
     '/index.js',
     '/styles.css',
@@ -25,7 +90,7 @@ self.addEventListener('activate', function(event) {
           // return array of cache names that are old to delete
           return Promise.all(
             cacheNames.map(key => {
-              if (key !== CACHE_NAME) {
+              if (key !== STATIC_CACHE) {
                 console.log('Removing old data', key);
               }
             })
@@ -45,7 +110,7 @@ self.addEventListener('activate', function(event) {
   
   self.addEventListener('fetch', function(event) {
     event.respondWith(
-      caches.open(CACHE_NAME).then(cache => {
+      caches.open(STATIC_CACHE).then(cache => {
         return cache.match(event.request).then(response => {
           return response || fetch(event.request);
         });
