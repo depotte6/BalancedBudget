a1bb30400fd38ab05502659adbe2619424d35d54
depotte6 <depotte6@gmail.com>
2022-01-19 03:08:35 -0500

    heroku fixes

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = process.env.PORT || 3001;
+const PORT = 3001;
 
 const app = express();
 
@@ -15,12 +15,10 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/BudgetStore',
+mongoose.connect("mongodb://localhost/BudgetStore",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true,
     useFindAndModify: false
   }
 );
